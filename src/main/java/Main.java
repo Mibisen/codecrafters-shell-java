f7053ef4d9b9a4d11ea795b57f5fcb67a7b34fcb
Mibisen <133812968+Mibisen@users.noreply.github.com>
2026-05-22 23:34:07 +0630

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,19 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            if (input.equals("exit")) {
-                break;
-            }
-                System.out.println(input + ": command not found");
+                if (input.equals("exit")) 
+                {
+                    break;
+                }
+                else if (input.startsWith("echo ")) 
+                {
+                    System.out.println(input.substring(5));
+
+                }
+                else 
+                {
+                    System.out.println(input + ": command not found");
+                }
         }
     }
 }
