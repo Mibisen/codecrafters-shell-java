6b03cbc6575b4c6399f08445fc214f5e176b85be
Mibisen <133812968+Mibisen@users.noreply.github.com>
2026-05-23 08:32:04 +0630

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,7 +24,7 @@ public class Main {
                     }              
                     else 
                     {
-                        System.out.println(input + ": command not found");
+                        System.out.println(input.substring(5) + ": not found");
                     }                         
                 }
                 else 
